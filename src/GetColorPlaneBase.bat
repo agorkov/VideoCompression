28cba66e4fad5d39ac3e60410344dd990a5ed815
Alexey <gorkoff@gmail.com>
2013-10-15 19:12:14 +0400

    Скрипт для автоматического сбора баз в разных цветовых пространства�

---
+++ src/GetColorPlaneBase.bat
@@ -0,0 +1,13 @@
+GetBase %1 d RGB.R -W -M
+rename %1.base %1.RGB.R.base
+GetBase %1 d RGB.G -W -M
+rename %1.base %1.RGB.G.base
+GetBase %1 d RGB.B -W -M
+rename %1.base %1.RGB.B.base
+
+GetBase %1 d YIQ.Y -W -M
+rename %1.base %1.YIQ.Y.base
+GetBase %1 d YIQ.I -W -M
+rename %1.base %1.YIQ.I.base
+GetBase %1 d YIQ.Q -W -M
+rename %1.base %1.YIQ.Q.base
